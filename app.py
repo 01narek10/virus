c19fd1352675916d45559b4bd332f583b798533a
01narek10 <01narek10nersisyan@gmail.com>
2026-03-08 23:35:52 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect
 from datetime import datetime
-import google.genai as genai
+import google.generativeai as genai
 import os
 
 app = Flask(__name__)
