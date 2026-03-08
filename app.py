c1038d3c34a3af02a4639c52f25ed1f6574d4684
01narek10 <01narek10nersisyan@gmail.com>
2026-03-08 23:58:02 +0400

    Update app.py

--- app.py
+++ app.py
@@ -276,6 +276,6 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
-    import os
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
