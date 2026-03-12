cb09b9af7051aa300aa3ce2cd39c02c8cd4943b6
01narek10 <01narek10nersisyan@gmail.com>
2026-03-12 22:20:04 +0400

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,10 @@ questions_db = {
 }
 
 # ==================== ԵՐԹՈՒԹՅՈՒՆՆԵՐ ====================
+@app.route("/health")
+def health():
+    return "OK", 200
+    
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -219,3 +223,4 @@ with app.app_context():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
