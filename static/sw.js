6903c2fbd4f96c8d1a80443ce30dc34bba2f5029
01narek10 <01narek10nersisyan@gmail.com>
2026-04-04 22:54:26 +0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,34 @@
+const CACHE_NAME = 'virus-encyclopedia-v1';
+const urlsToCache = [
+  '/',
+  '/static/style.css',
+  '/static/favicon.png',
+  '/static/icon-192.png',
+  '/static/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
